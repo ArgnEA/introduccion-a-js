e4d1888dd788b369f323434f9d755320ee15d4eb
Aragon Emanuel <aragon3m4@gmail.com>
2022-02-08 10:22:49 -0300

    Completed Tarea 2 in tarea-clase-3.js

--- tareas/clase-3/tarea-clase-3.js
+++ tareas/clase-3/tarea-clase-3.js
@@ -6,6 +6,7 @@
 // Si el nombe del usuario es el mismo que el que nombre que eligieron
 // Imprimir "Hola " y el nombre, " te llamás igual que mi ..."
 // Si no, simplemente imprimir "Hola " + nombre!
+
 const myName = `Emanuel`
 const userName = prompt(`¿Cual es tu nombre?`);
 const friendName = `Nick`
@@ -20,6 +21,16 @@ console.log(`Hola, ${userName}`)
 //Tarea 2:
 // Preguntar la edad del usuario
 // Hacerle saber si tiene más, menos ó la misma edad que nosotros.
+
+const myAge = 21;
+const userAge = Number(prompt(`¿Cual es tu edad?`));
+if (myAge === userAge) {
+    console.log(`Tenemoos la misma edad!`);   
+}else if(myAge < userAge){
+    console.log(`Sos mayor que yo`);
+}else if (myAge > userAge) {
+    console.log(`Sos menor que yo`)   
+} 
 
 //Tarea 3:
 // Preguntarle al usuario si tiene documento, y que conteste con "si" o "no".
