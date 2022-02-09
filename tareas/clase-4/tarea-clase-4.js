d9f9eda3d1355d9799e5ceedfa6fe7b75650950f
Aragon Emanuel <aragon3m4@gmail.com>
2022-02-09 15:01:55 -0300

    Completed tarea-clase-4.js

--- tareas/clase-4/tarea-clase-4.js
+++ tareas/clase-4/tarea-clase-4.js
@@ -1,9 +1,18 @@
 // TAREA: Imprimí cada 3er número del 3 al 22 usando un 'bucle for'.
 
+for (let i = 3; i <= 22; i++){
+    console.log(i);
+   
+}
 
 // TAREA: Usando un bucle 'while', decile a tu computadora que registre los números de
 //       diez a uno.
 
+let i = 11;
+while (i > 1) {
+    i--;
+    console.log(i);
+}
 
 // TAREA: Ha llegado el momento de un ejercicio clásico: 'FizzBuzz'.
 // Cuenta del 1 al 50 e imprime los números:
@@ -17,5 +26,25 @@
 // 16% 4 = 0 - en 16 tenemos 4 * 4
 // 19% 4 = 3 - en 19 tenemos 4 * 4 + 3, etc.
 
+for(let i=1; i<=100; i++) 
+			if ((i%3 == 0) && (i%5 == 0))
+				console.log(`FizzBuzz`);
+			else if (i%3 == 0)
+                    console.log(`Fizz`);
+				else if (i%5 == 0)
+                        console.log(`Buzz`);
+					else 
+                        console.log(i);
+
 // Calcular el promedio de todos los números en un array de números. (y ponerlo en una función)
 // ej.: calcularPromedio([10,5,4,2,8])
+
+console.log(calcularPromedio([10,5,4,2,8]));
+
+function calcularPromedio(array){
+    let promedio = 0;
+    for (let i = 0; i < array.length; i++) {
+        promedio += array[i];
+    }
+    return promedio / array.length;
+}
